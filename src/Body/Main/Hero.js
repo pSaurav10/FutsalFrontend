c772b360a4701064cccb708109ecf581e95e37df
pSaurav10 <sauravinmail@gmail.com>
2021-03-24 17:27:16 +0545

    Hero Section changed

--- src/Body/Main/Hero.js
+++ src/Body/Main/Hero.js
@@ -6,9 +6,8 @@ class Hero extends Component{
         return(
             <section id="hero" class="d-flex align-items-center full-height">
             <div class="container text-center position-relative" data-aos="fade-in" data-aos-delay="200">
-              <h1>Your New Online Presence with Bethany</h1>
-              <h2>We are team of talented designers making websites with Bootstrap</h2>
-              <a href="#about" class="btn-get-started scrollto">Get Started</a>
+              <h1>Your New Online Presence with Futsal</h1>
+              <h2>Book futsals, Navigate events, Communicate with fellow players</h2>
             </div>
           </section>
         )
