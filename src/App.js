f9ad36e3987aa03a37375398593b1f437b9fb890
pSaurav10 <sauravinmail@gmail.com>
2021-03-27 20:27:57 +0545

    Responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from 'react-router-dom';
 import Header from './Header/Header';
 import Body from './Body/Body';
 import Footer from './Footer/Footer';
 import BacktoTop from './BacktoTop/BacktoTop';
-import Header1 from './Header/Header1';
 function App() {
   return (
     <BrowserRouter>
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,29 +2,73 @@ import { Component } from "react";
 
 class Header extends Component {
     render() {
+        if (localStorage.getItem('token')) {
+            var menu =  
+            <ul class="navbar-nav ml-auto navbar-center">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Home</a>
+            </li> 
+              <li class="nav-item">
+                <a class="nav-link"  href="/Futsal">Futsal</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="/Event">Events</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="/Community">Community</a>
+              </li>
+              <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                  <img src="static/databaseimage/{{ user.tbluser.profilephoto }}" width="30" height="30" class="rounded-circle"/>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                  <a class="dropdown-item" href="#">Dashboard</a>
+                                  <a class="dropdown-item" href="#">Profile</a>
+                                  <a class="dropdown-item " href="#">Log Out</a>
+                                </div>
+                              </li>
+              </ul>
+        }
+        else {
+            var menu = 
+            <ul class="navbar-nav ml-auto navbar-center">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Home</a>
+            </li> 
+              <li class="nav-item">
+                <a class="nav-link"  href="/Futsal">Futsal</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="/Event">Events</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="/Community">Community</a>
+              </li>
+              <li class="nav-item">
+                                <a class="nav-link get-started" href="/login">Login</a>
+                              </li>
+              </ul>
+        }
         return (
+            <div class="navigationbar">
+                
+            <nav class="navbar navbar-expand-lg fixed-top text-center">
+            <div class="container">
+              <a class="navbar-brand" href="home">
+                <img src="assets/img/futsallogo.png" height="50" alt=""/>
+              </a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
-            <header id="header" class="fixed-top d-flex align-items-center">
-                <div class="container">
-                    <div class="header-container d-flex align-items-center">
-                        <div class="logo mr-auto">
-                            <h1 class="text-light"><a href="/"><span>Futsal</span></a></h1>
-                        </div>
-                        {/* <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>--> */}
-                    
-                    <nav class="nav-menu d-none d-lg-block ">
-                        <ul>
-                            <li class="active"><a href="/">Home</a></li>
-                            <li><a href="/Futsal">Futsal</a></li>
-                            <li><a href="/Event">Events</a></li>
-                            <li><a href="/Community">Community</a></li>
-                            <li class="get-started"><a href="/Login">Login</a></li>
-                        </ul>
-                    </nav>
+                <i class='bx bx-menu' style={{color : "#009970"}}></i>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        {menu}
                 </div>
-              </div>
-              </header>
+                </div>
+            </nav>
             
+            </div> 
+
         )
     }
 }
